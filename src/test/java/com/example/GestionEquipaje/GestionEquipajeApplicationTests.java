ee5ad130b6898fc68b5b44c581b3d928b355d809
sebastianmora3 <sebastian.mora3@udea.edu.co>
2024-10-20 18:31:26 -0500

    add test

--- src/test/java/com/example/GestionEquipaje/GestionEquipajeApplicationTests.java
+++ src/test/java/com/example/GestionEquipaje/GestionEquipajeApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.GestionEquipaje;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestionEquipajeApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
